9c12965015987129b39f85944890dc061bc679af
Kovalchuk-Alexandr <mo250568kaa@gmail.com>
2023-10-18 10:22:24 +0300

    added lang switch

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark").matche
 }
 
 
-// Проверка темнойтемы в localStorage
+// Проверка темной темы в localStorage
 if (localStorage.getItem('darkMode') === 'dark') {
     btnDarkMode.classList.add("dark-mode-btn--active");
     document.body.classList.add('dark');
@@ -20,7 +20,6 @@ if (localStorage.getItem('darkMode') === 'dark') {
     btnDarkMode.classList.remove("dark-mode-btn--active");
     document.body.classList.remove('dark');
 }
-
 
 
 // Если меняются системные настройки (если автоматически в течении дня система
@@ -44,21 +43,106 @@ window
         }
 })
 
-    
+
 // Включение ночного режима по кнопке
 btnDarkMode.onclick = () => {
     btnDarkMode.classList.toggle("dark-mode-btn--active");
     
     // Определяем, какой режим включен
     const isDark = document.body.classList.toggle("dark");
-
+    
     // Сохраняем режим в localStorage
     if (isDark) {
         localStorage.setItem('darkMode', 'dark');
     } else {
         localStorage.setItem('darkMode', 'light');
-
+        
     }
+}
+
+// Работа с блоком 'lang' - переключение языка
+const lang = document.querySelectorAll(".lang__item");
+var langActive = document.querySelector(".lang__item--active");
+// const demo = document.querySelector(".demo");
+
+var langActiveName = langActive.innerHTML;
+
+// Если есть на localStorage сохраненный язык - устанавливаем
+if (localStorage.getItem('lang') != undefined) {
+
+    // Устанавливаем активный язык из хранилища
+    langActiveName = localStorage.getItem('lang');
+
+    // if (localStorage.getItem('darkMode') === 'dark') {
+    //     btnDarkMode.classList.add("dark-mode-btn--active");
+    //     document.body.classList.add('dark');
+    // } else if (localStorage.getItem('darkMode') === 'light') {
+    //     btnDarkMode.classList.remove("dark-mode-btn--active");
+    //     document.body.classList.remove('dark');
+    // }
+} else {
+    // Устанавливаем язык по-умолчанию 'RU'
+    langActiveName = 'RU';
+    localStorage.setItem('lang', langActiveName);
+}
+
+
+
+
+// Проверяем клик
+lang.forEach(el => {
+
+    if (el.innerHTML == langActiveName) {
+        // Если, существующий язык и выбранный не совпадают, меняем активный
+        if (!el.classList.contains("lang__item--active")) {
+
+            // Удаляем класс ".active" у существующего элемента
+            langActive.classList.remove("lang__item--active");
+    
+            // и добавляем к выбранному
+            el.classList.add("lang__item--active");
+    
+            // Сохраняем режим в localStorage
+            localStorage.setItem('lang', langActiveName);
+        }
+    }
+    // let langActiveName = el.innerHTML;
+    el.addEventListener("click", () => {
+        // demo.innerHTML = "Click!!! from forEach";
+        const langActive = document.querySelector(".lang__item--active");
+        langActiveName = el.innerHTML;
+
+        // Если, существующий язык и выбранный не совпадают, меняем активный
+        if (!el.classList.contains("lang__item--active")) {
+
+            // Удаляем класс ".active" у существующего элемента
+            langActive.classList.remove("lang__item--active");
+    
+            // и добавляем к выбранному
+            el.classList.add("lang__item--active");
+    
+            // Сохраняем режим в localStorage
+            localStorage.setItem('lang', langActiveName);
+        }
+
+    })
+})
+
+lang.onclick = (el) => {
+    demo.innerHTML = "Click!!! from lang";
+    let langActive = el.target;
+    // btnDarkMode.classList.toggle("dark-mode-btn--active");
+    
+    // Определяем, какой режим включен
+    // const isDark = document.body.classList.toggle("dark");
+
+    // Сохраняем режим в localStorage
+    // if (isDark) {
+    //     localStorage.setItem('darkMode', 'dark');
+    // } else {
+    //     localStorage.setItem('darkMode', 'light');
+
+    // }
 }
 
 
--- js/projects.js
+++ js/projects.js
@@ -1,44 +1,74 @@
 const projects = [
     {
         id: 0,
-        title: 'Gaming streaming portal',
-        desc: 'Skills: React, Node.js, MongoDB',
-        img: '01.jpg',
-        imgBig: '01-big.jpg'
+        title: 'Верстка сайта «Free Health»',
+        desc: 'Анимация, выплывающее сбоку меню, Header появляется после прокрутки 50% экрана.',
+        skills: 'jQuery',
+        img: 'fh_sm.jpg',
+        imgBig: 'fh_lg.jpg',
+        site: 'https://kovalchuk-alexandr.github.io/Free-Health/',
+        siteTitle: 'Free-Health',
+        gitRef: '',
+        gitTitle: 'Free-Health'
     },
     {
         id: 1,
-        title: 'Video service',
-        desc: 'Skills: Media, Node.js, MongoDB',
-        img: '02.jpg',
-        imgBig: '02-big.jpg'
+        title: 'Новостной портал ‘Blog’',
+        desc: 'Вывод всех статей, переход на подробное описание, на той же странице без перезагрузки. Форма комментариев, с добавлением/удалением, подсчет кол-ва, вывод сообщения, если нет. ',
+        skills: '(MEVN), client – Vue JS, server - Mongo DB',
+        img: 'blog_sm.jpg',
+        imgBig: 'blog_lg.jpg',
+        site: '',
+        siteTitle: 'Free Health',
+        gitRef: 'https://github.com/Kovalchuk-Alexandr/Blog-MEVN',
+        gitTitle: 'Web Blog'
     },
     {
         id: 2,
-        title: 'Video portal',
-        desc: 'Skills: Video, VueJS, Node.js, MongoDB',
-        img: '03.jpg',
-        imgBig: '03-big.jpg'
+        title: 'Интернет-магазин ',
+        desc: 'Корзина товаров, появляющаяся сбоку, добавление товаров не больше, чем на складе, удаление, авто-подсчет количества и стоимости, фильтр по категориям, поиск по названию, переход на страницу товара, анимация.',
+        skills: 'client - Java Script, server - Mongo DB',
+        img: 'e-shop_sm.jpg',
+        imgBig: 'e-shop_lg.jpg',
+        site: 'https://kovalchuk-alexandr.github.io/eshop_local/',
+        siteTitle: 'E-Shop',
+        gitRef: 'https://github.com/Kovalchuk-Alexandr/eshop-js.git',
+        gitTitle: 'E-Shop'
     },
     {
         id: 3,
-        title: 'Dating app',
-        desc: 'Skills: ExpressJS, TypeScript, Node.js, MongoDB',
-        img: '04.jpg',
-        imgBig: '04-big.jpg'
+        title: 'Freelancer portfolio ',
+        desc: 'Многостраничный сайт-портфолио. "Подробно". Светлая/Темная темы. Адаптив',
+        skills: 'Java Script',
+        img: 'fl_portfolio_sm.jpg',
+        imgBig: 'fl_portfolio_lg.jpg',
+        site: 'https://kovalchuk-alexandr.github.io/freelancer_portfolio_my/',
+        siteTitle: 'Freelancer portfolio',
+        gitRef: '',
+        gitTitle: ''
     },
     {
         id: 4,
-        title: 'Landing',
-        desc: 'Skills: React, HTML, CSS, JS',
-        img: '05.jpg',
-        imgBig: '05-big.jpg'
+        title: 'Portfolio ',
+        desc: 'Одностраничный сайт-портфолио. "Подробно". Адаптив.',
+        skills: 'Java Script',
+        img: 'portfolio1_sm.jpg',
+        imgBig: 'portfolio1_lg.jpg',
+        site: 'https://kovalchuk-alexandr.github.io/freelancer_portfolio_my/',
+        siteTitle: 'Freelancer portfolio',
+        gitRef: '',
+        gitTitle: ''
     },
     {
         id: 5,
-        title: 'Gaming comunity',
-        desc: 'Skills: Angular, Node.js, MongoDB',
-        img: '06.jpg',
-        imgBig: '06-big.jpg'
-    }
- ]
+        title: 'Простой сайт на jQuery ',
+        desc: 'Одностраничный сайт. Карусель. Боковое меню. Анимация',
+        skills: 'jQuery',
+        img: 'jquery_sm.jpg',
+        imgBig: 'jquery_lg.jpg',
+        site: 'https://kovalchuk-alexandr.github.io/jQuery/',
+        siteTitle: 'Washoo',
+        gitRef: 'https://github.com/Kovalchuk-Alexandr/jQuery.git',
+        gitTitle: 'Washoo'
+    },
+]
